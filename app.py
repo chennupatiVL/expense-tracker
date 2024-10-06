e9ef4979e1fb45160d53fbed8928382dd29559cc
chennupatiVL <chennupativijayalakshmi9@gmail.com>
2024-10-06 10:13:01 +0000

    fix bugs

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
+
 
 
 app=Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
 
