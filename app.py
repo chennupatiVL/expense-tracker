271895380a4a63dddaf2294189b70d331be8fea6
chennupatiVL <chennupativijayalakshmi9@gmail.com>
2024-10-07 07:24:16 +0000

    Updated Home completely

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route("/")
-def hello():
+def Home():
     return render_template("home.html")
+
+@app.route("/signIn")
+def signUp():
+    return render_template("signIn.html")
 
 
 
